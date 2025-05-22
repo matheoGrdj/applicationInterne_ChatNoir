a72a2a3b795e45cfb9a50498c6e562fb07d55d3a
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-05-22 10:44:03 +0200

    modifs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: process.env.NODE_ENV !== "production",
+  },
   css: ["@/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
