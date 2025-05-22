5aa354b7a97c45a3646d2629ea1295a372ca21d0
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-05-22 11:31:13 +0200

    ajout de log d'erreur

--- server/api/images.get.js
+++ server/api/images.get.js
@@ -2,8 +2,17 @@ import { readFile } from 'fs/promises'
 import { join } from 'path'
 
 export default defineEventHandler(async () => {
-  // Utiliser le chemin absolu pour accéder au fichier
-  const filePath = join(process.cwd(), 'public', 'data', 'images.json')
-  const data = await readFile(filePath, 'utf-8')
-  return JSON.parse(data)
+  try {
+    // Utiliser le chemin absolu pour accéder au fichier
+    const filePath = join(process.cwd(), 'public', 'data', 'images.json')
+    console.log('Chemin du fichier :', filePath) // Log du chemin du fichier
+    const data = await readFile(filePath, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    console.error('Erreur lors de la lecture du fichier :', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Erreur lors de la lecture du fichier',
+    })
+  }
 })
