0e7b05daded7845568bf2ae4c44c07b4a6c60f14
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-05-22 11:36:41 +0200

    modif

--- server/api/images.get.js
+++ server/api/images.get.js
@@ -5,11 +5,11 @@ export default defineEventHandler(async () => {
   try {
     // Utiliser un chemin relatif pour accéder au fichier
     const filePath = join(__dirname, '..', 'data', 'images.json')
-    console.log('Chemin du fichier :', filePath) // Log du chemin du fichier
+    console.log('Chemin du fichier :', filePath)
     const data = await readFile(filePath, 'utf-8')
     return JSON.parse(data)
   } catch (error) {
-    console.error('Erreur lors de la lecture du fichier :', error)
+    console.error('Erreur lors de la lecture du fichier :', filePath, error)
     throw createError({
       statusCode: 500,
       statusMessage: 'Erreur lors de la lecture du fichier',
