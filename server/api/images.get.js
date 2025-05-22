f892768b28e8ebacb109c0cb493f803c8f901319
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-05-22 11:34:27 +0200

    modif

--- server/api/images.get.js
+++ server/api/images.get.js
@@ -3,8 +3,8 @@ import { join } from 'path'
 
 export default defineEventHandler(async () => {
   try {
-    // Utiliser le chemin absolu pour accéder au fichier
-    const filePath = join(process.cwd(), 'public', 'data', 'images.json')
+    // Utiliser un chemin relatif pour accéder au fichier
+    const filePath = join(__dirname, '..', 'data', 'images.json')
     console.log('Chemin du fichier :', filePath) // Log du chemin du fichier
     const data = await readFile(filePath, 'utf-8')
     return JSON.parse(data)
